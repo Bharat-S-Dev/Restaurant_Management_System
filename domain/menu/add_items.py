444881dc8f1e86df4622bc6ee91419da998ebc16
Bharat Singh <bharatsjadon@gmail.com>
2026-03-09 03:37:53 +0530

    improve menu loading and saving
    Refactored add_menu function to use load_menu and save_menu_to_file for better menu management.

--- domain/menu/add_items.py
+++ domain/menu/add_items.py
@@ -1,72 +1,4 @@
-# import json
-# # from domain.menu.menu_data import MENU, save_menu_to_file
 
-# with open("menu.json", "r") as file:
-#     MENU = json.load(file)
-
-# def add_menu():
-#     item_id = input("Enter new item ID: ").upper()
-
-#     if any(item['item_id'] == item_id for item in MENU):
-#         print("Item ID already exists.")
-#         return
-
-#     name = input("Enter item name: ").capitalize()
-#     category = input("Enter category(Breakfast/Lunch...): ").capitalize()
-#     options = {}
-
-#     while True:
-#         size = input("Enter portion size (full/half): ").lower()
-#         price = float(input(f"Enter price for {size}: "))
-#         options[size] = price
-
-#         if input("Add another size? (y/n): ").lower() != "y":
-#             break
-
-#     new_item = {
-#         "item_id": item_id,
-#         "name": name,
-#         "category": category,
-#         "options": options
-#     }
-
-#     MENU.append(new_item)
-#     print(f"{name} added to the menu under {category}.")
-
-#     with open("menu.json", "w") as file:
-#         json.dump(MENU, file, indent=4)
-#         print("Menu updated and saved.")
-###############################
-
-# import json
-# from domain.menu.menu_data import MENU, save_menu_to_file
-
-# def add_menu():
-#     item_id = input("Enter new item ID: ").upper()
-#     if any(item['item_id'] == item_id for item in MENU):
-#         print("Item ID already exists.")
-#         return
-    
-#     name = input("Enter item Name: ").capitalize()
-#     category = input("Enter Category (Breakfast/Lunch...): ").capitalize()
-#     options = {}
-#     while True:
-#         size = input("Enter portion size (full/half): ").lower()
-#         price = float(input(f"Enter price for {size}: "))
-#         options[size] = price
-#         if input("Add another size? (y/n): ").lower() != 'y':
-#             break
-
-#     MENU.append({
-#         "item_id": item_id,
-#         "name": name,
-#         "category": category,
-#         "options": options
-#     })
-#     print(f"{name} added to the menu under {category}.")
-#     save_menu_to_file()
-
-# #################################################
 from domain.menu.menu_data import load_menu, save_menu_to_file
 
 def add_menu():
@@ -107,6 +39,7 @@ def add_menu():
 
 
 ################################################
+
 ########################3
 # from domain.menu.menu_data import load_menu, save_menu_to_file
 
@@ -135,3 +68,5 @@ def add_menu():
 #     menu.append(new_item)
 #     save_menu_to_file(menu)
 #     print(f"{name} added to menu.")
+
+
